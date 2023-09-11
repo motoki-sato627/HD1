90353ec845faf4afa1e6349a9d1899d98c3d675c
motoki-sato627 <141602953+motoki-sato627@users.noreply.github.com>
2023-09-11 17:24:45 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,17 +7,19 @@ def sample(a, b, a_, b_):
     with open('/Users/satomotoki/Desktop/modified-swiss-dwellings-v1-train_50/graph_out_50/'+str(num)+'.pickle', 'rb') as file:
         G = pickle.load(file)
     n = G.number_of_nodes()
-    coords=[]
-    rms=[]
+    coords = []
+    rms = []
+    edges=[]
     for i in range(n):
-      poly = Polygon(G.nodes[i]['geometry'])
-      coord = list(poly.exterior.coords)
-      l=len(coord)
-      e=np.random.normal(0, 1, (l,2))
-      e[-1]=e[0]
-      coords.append(list(e))
-      rm = G.nodes[i]['room_type']
-      rms.append(rm)
+        poly = Polygon(G.nodes[i]['geometry'])
+        coord = list(poly.exterior.coords)
+        edges.append(len(coord)-1)
+        for j in range(len(coord)-1):
+            coords.append(coord)
+            rm = G.nodes[i]['room_type']
+            rms.append(rm)
+    coords=np.array(coords)
+    e=
     for t in range(T):
       t=1000-t
       features=embedding(t, coords, rms)
