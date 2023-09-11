f76c49a524b44ab2c5915e12461c4579cc1ee2e0
motoki-sato627 <141602953+motoki-sato627@users.noreply.github.com>
2023-09-11 14:19:45 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,21 @@
 import tensorflow as tf
 from nn import define_model, params
+from feature_embedding import data_load, embedding
+
+def train(model, a, b, a_, b_, T, ori_coords, rms):
+  L=ori_coors.shape[0]
+  for t in renge(T):
+    e=np.random.normal(0, 1, (L,2))
+    features=embedding(
+    
 
 def main():
   T=1000
   model=define_model()
   a, b, a_, b_=params(T)
-  train(model, a, b, a_, b_, T)
+  for num in range(1):
+    ori_coodrs, rms=data_load(num)
+    train(model, a, b, a_, b_, T, ori_coodrs, rms)
+
+if __name__ == "__main__":
+    main()
