d9eaf05cb1d8276fc759ecc6f8070ec158c34f10
motoki-sato627 <141602953+motoki-sato627@users.noreply.github.com>
2023-09-11 14:49:13 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,13 @@
-def sample():
+def sample(a, b, a_, b_):
   for num in range(1):
+    with open('/Users/satomotoki/Desktop/modified-swiss-dwellings-v1-train_50/graph_out_50/'+str(num)+'.pickle', 'rb') as file:
+        G = pickle.load(file)
+    G = data_preprocess.preprocess(G)
+    n = G.number_of_nodes()
+    for i in range(n):
+        poly = Polygon(G.nodes[i]['geometry'])
+        coord = list(poly.exterior.coords)
+        coords.append(coord)
+        rm = G.nodes[i]['room_type']
+        rms.append(rm)
     
