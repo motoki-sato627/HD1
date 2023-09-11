416dfbba6f147242635dec1a14213a3fd1777208
motoki-sato627 <141602953+motoki-sato627@users.noreply.github.com>
2023-09-11 17:22:22 +0900

    Update train.py

--- train.py
+++ train.py
@@ -2,17 +2,12 @@ import tensorflow as tf
 from nn import define_model, params
 from feature_embedding import data_load, embedding
 
-def train(model, a, b, a_, b_, T, ori_coords, rms):
-  L=len(ori_coors)
+def train(model, a, b, a_, b_, T, ori_coords, rms, edges, n):
   for t in renge(T):
     t+=1
-    coords=[]
-    for i in range(L):
-      e=np.random.normal(0, 1, (len(ori_coords[i]),2))
-      e=list(e)
-      coords.append(np.sqrt(a_[t])*ori_coords[i]+np.sqrt(b_[t])*e)
-    coords[:][-1]=coords[:][0]
-    features=embedding(t, coords, rms)
+    e=np.random.normal(0,1,(ori_coords.shape[0], 2))
+    coords=np.sqrt(a_[t])*ori_coords[i]+np.sqrt(b_[t])*e
+    features=embedding(t, coords, rms, edges, n)
     model.fit(features, e, epochs=100, batch_size=min(32,L))
   model.save("/Users/satomotoki/Desktop/model/file")
   
@@ -23,8 +18,8 @@ def main():
   model=define_model()
   a, b, a_, b_=params(T)
   for num in range(1):
-    ori_coodrs, rms=data_load(num)
-    train(model, a, b, a_, b_, T, ori_coodrs, rms)
+    ori_coodrs, rms, edges, n=data_load(num)
+    train(model, a, b, a_, b_, T, ori_coodrs, rms, edges, n)
 
 if __name__ == "__main__":
     main()
