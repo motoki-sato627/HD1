35a456f45fa47d2215d89a872fd1436f6e47013b
motoki-sato627 <141602953+motoki-sato627@users.noreply.github.com>
2023-09-11 16:58:56 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -20,6 +20,23 @@ def sample(a, b, a_, b_):
       rms.append(rm)
     for t in range(T):
       t=1000-t
-      
-      
-    
+      features=embedding(t, coords, rms)
+      pre_e=model.predict(features)
+      e_coords=[]
+      u_coords=[]
+      cnt=0
+      for i in range(n):
+        e=[]
+        u=p.random.normal(0, 1, (len(coords[i]),2))
+        u[-1]=u[0]
+        for j in range(len(coords[i])-1):
+          e.append(list(pre_e[cnt]))
+          cnt+=1
+        e.append(e[0])
+        e_coords.append(e)
+        u_coords.append(u)
+      if t==1:
+        coords=(coords-e_coords*b[t]/np.sqrt(b_[t]))
+      else:
+        coords=(coords-e_coords*b[t]/np.sqrt(b_[t]))+b[t]*u_coords
+    vi
