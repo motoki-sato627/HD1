448e68868c72d6812c950871e9318382d7627497
motoki-sato627 <141602953+motoki-sato627@users.noreply.github.com>
2023-09-11 14:54:25 +0900

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,8 @@ from nn import define_model, params
 from feature_embedding import data_load, embedding
 
 def train(model, a, b, a_, b_, T, ori_coords, rms):
-  L=ori_coors.shape[0]
+  L1=ori_coors.shape[0]
+  L2=ori_coors.shape[1]
   for t in renge(T):
     e=np.random.normal(0, 1, (L,2))
     coords=np.sqrt(a_[t])*ori_coords+np.sqrt(b_[t])*e
